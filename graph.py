938c91806e9d4eeb25dc1b2104ca7917d5aeec5e
alaniszhao <zhaoalanis@gmail.com>
2024-11-14 14:51:05 -0500

    code showing directions

--- graph.py
+++ graph.py
@@ -4,75 +4,87 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
-df = pd.read_csv('2021_12_08_17_31_26_933bcb2ee1c266e0.csv')
+df = pd.read_csv('2022_06_06_02_05_21_842e2ba4c5a6fb19.csv')
 
 latitude = df['latitude']
 longitude = df['longitude']
 timestamp_offset = df['timestamp_offset']
+compass = df['compass']
+
+step = 10
+latitude = latitude[::step]
+longitude = longitude[::step]
+compass = compass[::step]
+timestamp_offset = timestamp_offset[::step]
 
 plt.figure(figsize=(10, 6))
 scatter = plt.scatter(longitude, latitude, c=timestamp_offset, cmap='viridis', s=50, alpha=0.8)
-
 plt.colorbar(scatter, label='Timestamp Offset')
-plt.ticklabel_format(style='plain', axis='both', useOffset=False)
 
+compass_rad = np.deg2rad(compass)
+u = np.cos(compass_rad)
+v = np.sin(compass_rad)
+plt.quiver(longitude, latitude, u, v, angles='xy', scale_units='xy', scale=20000, color='red', alpha=0.6)
+
+
+plt.ticklabel_format(style='plain', axis='both', useOffset=False)
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 
 plt.show()
 
 
+if False:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim(min(df['acc_x']), max(df['acc_x']))
+    ax.set_ylim(min(df['acc_y']), max(df['acc_y']))
+    ax.set_zlim(min(df['acc_z']), max(df['acc_z']))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim(min(df['acc_x']), max(df['acc_x']))
-ax.set_ylim(min(df['acc_y']), max(df['acc_y']))
-ax.set_zlim(min(df['acc_z']), max(df['acc_z']))
+    ax.set_xlabel('Acceleration X')
+    ax.set_ylabel('Acceleration Y')
+    ax.set_zlabel('Acceleration Z')
 
-ax.set_xlabel('Acceleration X')
-ax.set_ylabel('Acceleration Y')
-ax.set_zlabel('Acceleration Z')
+    point, = ax.plot([], [], [], 'o', color='blue')
 
-point, = ax.plot([], [], [], 'o', color='blue')
+    def update_acc(num):
+        point.set_data(df['acc_x'][num], df['acc_y'][num])
+        point.set_3d_properties(df['acc_z'][num])
+        return point,
 
-def update_acc(num):
-    point.set_data(df['acc_x'][num], df['acc_y'][num])
-    point.set_3d_properties(df['acc_z'][num])
-    return point,
+    ani = FuncAnimation(fig, update_acc, frames=len(df['timestamp_offset']), interval=50, blit=True, repeat=False)
 
-ani = FuncAnimation(fig, update_acc, frames=len(df['timestamp_offset']), interval=50, blit=True, repeat=False)
-
-plt.show()
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim(min(df['gyro_x']), max(df['gyro_x']))
-ax.set_ylim(min(df['gyro_y']), max(df['gyro_y']))
-ax.set_zlim(min(df['gyro_z']), max(df['gyro_z']))
-
-ax.set_xlabel('Gyroscope X')
-ax.set_ylabel('Gyroscope Y')
-ax.set_zlabel('Gyroscope Z')
-
-point, = ax.plot([], [], [], 'o', color='blue')
-
-def update_acc(num):
-    point.set_data(df['gyro_x'][num], df['gyro_y'][num])
-    point.set_3d_properties(df['gyro_z'][num])
-    return point,
-
-ani = FuncAnimation(fig, update_acc, frames=len(df['timestamp_offset']), interval=50, blit=True, repeat=False)
-
-plt.show()
+    plt.show()
 
 
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlim(min(df['gyro_x']), max(df['gyro_x']))
+    ax.set_ylim(min(df['gyro_y']), max(df['gyro_y']))
+    ax.set_zlim(min(df['gyro_z']), max(df['gyro_z']))
+
+    ax.set_xlabel('Gyroscope X')
+    ax.set_ylabel('Gyroscope Y')
+    ax.set_zlabel('Gyroscope Z')
+
+    point, = ax.plot([], [], [], 'o', color='blue')
+
+    def update_acc(num):
+        point.set_data(df['gyro_x'][num], df['gyro_y'][num])
+        point.set_3d_properties(df['gyro_z'][num])
+        return point,
+
+    ani = FuncAnimation(fig, update_acc, frames=len(df['timestamp_offset']), interval=50, blit=True, repeat=False)
+
+    plt.show()
 
 
-plt.figure(figsize=(10, 6))
-plt.plot(df['timestamp_offset'], df['compass'], label='compass', color='b')
-plt.xlabel('Timestamp Offset')
-plt.ylabel('Compass (degree)')
-plt.legend()
-plt.grid(True)
-plt.show()
+
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['timestamp_offset'], df['compass'], label='compass', color='b')
+    plt.xlabel('Timestamp Offset')
+    plt.ylabel('Compass (degree)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
--- predict.py
+++ predict.py
@@ -4,37 +4,43 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from haversine import haversine, Unit
 import os
+import statsmodels.api as sm
+from xgboost import XGBRegressor
 
 data_list = []
-rootdir = ''
+rootdir = './'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file_path in files:
-        if file_path[:-3]=='csv':
+        if file_path[-3:]=='csv' and file_path!='2021_12_27_13_48_08_c8503244bd97fd63.csv':
             data = pd.read_csv(file_path)
 
             data['displacement'] = data.apply(lambda row: haversine((row['latitude'], row['longitude']),
                                                                     (data['latitude'].shift(-1)[row.name], data['longitude'].shift(-1)[row.name]
                                                                     if row.name < len(data) - 1 else np.nan), 
-                                                                    Unit=Unit.METERS), axis=1) 
+                                                                    unit=Unit.INCHES), axis=1) 
 
             data = data.dropna(subset=['displacement'])
             data_list.append(data)
 
 data = pd.concat(data_list, ignore_index=True)
 
-X = data[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'compass']]
+X = data[['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z']]
 y = data['displacement']
 
 split_idx = int(0.8 * len(data))
 X_train, X_test = X[:split_idx], X[split_idx:]
 y_train, y_test = y[:split_idx], y[split_idx:]
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+model = XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1, max_depth=6)
 
+model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
+
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
+
+for i in range(len(y_pred)):
+    print('Predicted: ' + str(y_pred[i]) + ' Actual: '+str(y_test.iloc[i]))
 
 print(f"Root Mean Squared Error: {rmse}")
